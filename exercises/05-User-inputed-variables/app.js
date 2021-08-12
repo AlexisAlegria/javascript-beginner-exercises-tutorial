0dba502c1e2acc877a4fcc6e7571e34518f602af
AlexisAlegria <alexisalegriav@gmail.com>
2021-08-12 17:16:53 +0000

    up to exercise 15 compiled

--- exercises/05-User-inputed-variables/app.js
+++ exercises/05-User-inputed-variables/app.js
@@ -1,3 +1,5 @@
 var age = prompt('What is your age?');
 
 // Your code below:
+var calculateAge = parseInt(age) + 10;
+console.log(calculateAge);
--- exercises/15-Random-numbers/app.js
+++ exercises/15-Random-numbers/app.js
@@ -1,10 +1,10 @@
 /*Exercise closely resembles 11 - consider introducing another concept here? */
 
-function getRandomInt()
+function getRandomInt(a,b)
 {
-	var randomNumber = Math.random();
+	var randomNumber = Math.floor((Math.random() * b) + a);
 	return randomNumber;
 }
 
 
-console.log(getRandomInt());
+console.log(getRandomInt(1,10));
