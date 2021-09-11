dd67ebc1a55335633779dbd94b033876515786a6
AlexisAlegria <alexisalegriav@gmail.com>
2021-09-11 00:45:32 +0000

    exercise 20 fixed

--- exercises/20-Random-colors-loop/app.js
+++ exercises/20-Random-colors-loop/app.js
@@ -1,30 +1,29 @@
-function getColor(colorNumber)
+function getColor(colorNumber=0)
 {
-    //make sure parameter is a number and not a string by converting the value to int:
-    colorNumber = parseInt(colorNumber);
-    switch(colorNumber){
-        case 1: return "red"; 
-                break;
-        case 2: return "yellow"; 
-                break;
-        case 3: return "blue"; 
-                break;
-        case 4: return "green"; 
-                break;
-        default: return "black"; 
-                 break;
-    }
+	//make sure parameter is a number and not a string by converting the value to int:
+	colorNumber = parseInt(colorNumber);
+	switch(colorNumber){
+		case 1: return "red"; 
+				break;
+		case 2: return "yellow"; 
+				break;
+		case 3: return "blue"; 
+				break;
+		case 4: return "green"; 
+				break;
+		default: return "black"; 
+				 break;
+	}
 }
 
-function getAllStudentColors(){
-    
-    //your loop here
-    var exampleColor = getColor(1);
-
-    for(i=0; i < 10; i++){
-        exampleColor = Math.floor((Math.random() * 4) + 1);
-        console.log(getColor(exampleColor));
-    }
+function getAllStudentColors(n){
+	let array = [];
+	//your loop here
+		for(i=0; i < n; i++){
+			exampleColor = Math.floor((Math.random() * 4) + 1);
+			array.push(getColor(exampleColor));
+		}
+	return array;
 }
-//call the function below with the number of students in the class and print on the console
-getAllStudentColors();
+var output = getAllStudentColors(10);
+console.log(output);
